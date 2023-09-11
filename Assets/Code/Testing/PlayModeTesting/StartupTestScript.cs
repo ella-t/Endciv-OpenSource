6af9c25c77bddd93d11af40527c2378ec117199b
ella-t <58817053+ella-t@users.noreply.github.com>
2023-09-11 12:07:27 +0100

    Some startup test functions

--- Assets/Code/Testing/PlayModeTesting/StartupTestScript.cs
+++ Assets/Code/Testing/PlayModeTesting/StartupTestScript.cs
@@ -1,28 +1,54 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
+using Endciv;
 using NUnit.Framework;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.TestTools;
 
 namespace Tests
 {
     public class StartupTestScript
     {
-        // A Test behaves as an ordinary method
-        [Test]
-        public void StartupTestScriptSimplePasses()
+
+        [OneTimeSetUp]
+        public void LoadInitialScene()
         {
-            // Use the Assert class to test conditions
+            SceneManager.LoadScene(0);
+        }
+
+        // A Test behaves as an ordinary method
+        [Test, Order(1)]
+        public void LoadMainScene()
+        {
+            Debug.Log("Loading main scene");
         }
 
         // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
         // `yield return null;` to skip a frame.
-        [UnityTest]
-        public IEnumerator StartupTestScriptWithEnumeratorPasses()
+        [UnityTest, Order(2)]
+        public IEnumerator WaitForMainMenu()
         {
-            // Use the Assert class to test conditions.
-            // Use yield to skip a frame.
-            yield return null;
+            yield return new WaitUntil(() => SceneManager.GetActiveScene().name == "MainMenu");
+        }
+
+        [Test, Order(3)]
+        public void StartNewGame()
+        {
+            GameObject menuObject = GameObject.FindGameObjectWithTag("MainMenu");
+            ScenarioController scenario = menuObject.GetComponentInChildren<ScenarioController>(true);
+            scenario.OnStartGame();
+        }
+
+        [UnityTest, Order(4)]
+        public IEnumerator WaitForIngame()
+        {
+            yield return new WaitUntil(() => SceneManager.GetActiveScene().name == "Game");
+        }
+
+        [UnityTest, Order(5)]
+        public IEnumerator RunIngame()
+        {
+            yield return new WaitForSeconds(5);
         }
     }
 }
